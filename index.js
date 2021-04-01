2c2eba8d9a5a07093622127c08f6730e97215353
Mehul Mohan <mehulmpt@gmail.com>
2021-04-01 22:43:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
-// NOTE: YOU HAVE TO RUN THE SERVER ON PORT 1337 ONLY. OTHERWISE THE OUTPUT ON RIGHT WILL NOT WORK
-const port = process.env.PUBLIC_PORT || 1337
+// NOTE: YOU HAVE TO RUN THE SERVER ON THIS PORT ONLY. OTHERWISE THE OUTPUT ON RIGHT WILL NOT WORK
+const port = process.env.PUBLIC_PORT
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200
