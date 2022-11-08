cfc7c82d7d98312373fd5a330fbc0707e72d3a03
Pranav <pranav.techiegeek@gmail.com>
2022-11-08 23:48:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const http = require('http')
-
-// NOTE: YOU HAVE TO RUN THE SERVER ON THIS PORT ONLY. OTHERWISE THE OUTPUT ON RIGHT WILL NOT WORK
-const port = process.env.PUBLIC_PORT
-
-const server = http.createServer((req, res) => {
-	res.statusCode = 200
-	res.setHeader('Content-Type', 'text/plain')
-	res.end('Hello from codedamn!')
-})
-
-server.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}/`)
-})
+console.log("hello, world!");
